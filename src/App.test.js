f0512b87ec416673bd31d81a7637ebd3b5aa2647
Karthik Benki <karthikmc007@gmail.com>
2022-06-30 20:34:17 +0530

    [Karthik] Modify. move enzyme configuration to setupTest.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,6 @@ import { mount, shallow } from "enzyme";
 import App from "./App";
 
 
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-configure({ adapter: new Adapter() });
 
 
 describe("Counter testing", () => {
