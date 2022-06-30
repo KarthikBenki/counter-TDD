744cab629123643d1417077d849535d0adcd31fe
Karthik Benki <karthikmc007@gmail.com>
2022-06-30 20:06:50 +0530

    [Karthik] Add. test to check increment and decrement of counter value when respective button is clicked

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
     <div className="App">
         <h1>This is counter App</h1>
         <div id="counter-value">{counter}</div>
-        <button id="increment-btn">Increment</button> 
+        <button id="increment-btn" onClick={()=>setCounter(counter+1)}>Increment</button> 
+        <button id="decrement-btn" onClick={()=>setCounter(counter-1)}>Increment</button> 
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -26,7 +26,25 @@ describe("Counter testing", () => {
 
   it("Should render the initial value of state in div",()=>{
     const wrapperValue = wrapper.find("#counter-value").text();
-    
+
     expect(wrapperValue).toBe("0");
   })
+
+  it('Should render the click event if increment button and increment value', () => {
+    wrapper.find("#increment-btn").simulate('click');//clicks the button
+
+    const wrapperValue = wrapper.find("#counter-value").text();
+
+    expect(wrapperValue).toBe("1");
+  });
+
+  it('Should render the click event if decrement button and decrement value', () => {
+    wrapper.find("#increment-btn").simulate('click');//clicks the increment button
+
+    wrapper.find("#decrement-btn").simulate('click');//clicks the decrement button
+
+    const wrapperValue = wrapper.find("#counter-value").text();
+
+    expect(wrapperValue).toBe("0");
+  });
 })
